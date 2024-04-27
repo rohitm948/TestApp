0293167e9c5fbfb9592d14be9e0d698caeda0b76
rohitm948 <167735870+rohitm948@users.noreply.github.com>
2024-04-28 02:45:12 +0530

    First version code

---
+++ testapp/App.js
@@ -0,0 +1,18 @@
+import { View,Text, SafeAreaView, StyleSheet } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}><Text> Welcome Gazibos</Text></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+ 
+});
